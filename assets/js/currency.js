4a54c8d19660e05dec3eff16162c0989367cc5ee
vldkk <126193367+vldkk@users.noreply.github.com>
2023-02-23 21:44:44 +0000

    Add some cute changes

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -1,6 +1,7 @@
 (async () => {
     const container = document.querySelector('.intro__text-container');
-    const response = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
+    const response = await fetch("https://api.exchangerate-api.com/v4/latest/EUR");
     const data = await response.json();
-    container.innerHTML = `1 USD = ${data.rates.UAH} UAH`;
+    
+    container.innerHTML = `1 EUR = ${data.rates.UAH} UAH`;
 })();
